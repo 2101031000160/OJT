b50b57b103b5653ed11270bcbe9e73550a01eee3
2101031000160 <137051015+2101031000160@users.noreply.github.com>
2023-07-03 14:49:05 +0530

    php

---
+++ july 3/foreach.php
@@ -0,0 +1,11 @@
+
+<?php  
+    //declare array  
+    $season = array ("Summer", "Winter", "Autumn", "Rainy");  
+      
+    //access array elements using foreach loop  
+    foreach ($season as $element) {  
+        echo "$element";  
+        echo "</br>";  
+    }  
+?> 
